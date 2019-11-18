c01c3213ad57771ab2ea45fad65f2671ed1c2db9
Octavio Marchi Parisi <octaviomarchi@msn.com>
2019-11-18 14:08:09 -0800

    26. The Root of All Evil

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -7,36 +7,27 @@ import org.junit.jupiter.api.Test;
 
 public class MoneyTest {
 
-	@Test
-	void testMultiplicationDollar() {
-		Money five = Money.dollar(5);
-		assertEquals(Money.dollar(10), five.times(2));
-		assertEquals(Money.dollar(15), five.times(3));
-	}
-	
-	@Test
-	void testEqualityDollar() {
-		assertEquals(Money.dollar(5), Money.dollar(5));
-		assertNotEquals(Money.dollar(5), Money.dollar(8));
-		assertNotEquals(Money.dollar(5), Money.franc(5));
-	}
-	
-	@Test
-	void testMultiplicationFranc() {
-		Money five = Money.franc(5);
-		assertEquals(Money.franc(10), five.times(2));
-		assertEquals(Money.franc(15), five.times(3));
-	}
-	
-	@Test
-	void testEqualityFranc() {
-		assertEquals(Money.franc(5), Money.franc(5));
-		assertNotEquals(Money.franc(5), Money.franc(8));
-	}
-	
-	@Test
-	void testCurrency() {
-		assertEquals("USD", Money.dollar(1).currency());
-		assertEquals("CHF", Money.franc(1).currency());
-	}
+    @Test
+    void testMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+
+        Money fiveF = Money.franc(5);
+        assertEquals(Money.franc(10), fiveF.times(2));
+    }
+
+    @Test
+    void testEqualityDollar() {
+        assertEquals(Money.dollar(5), Money.dollar(5));
+        assertNotEquals(Money.dollar(5), Money.dollar(8));
+        assertEquals(Money.franc(5), Money.franc(5));
+        assertNotEquals(Money.dollar(5), Money.franc(5));
+    }
+
+    @Test
+    void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
 }
