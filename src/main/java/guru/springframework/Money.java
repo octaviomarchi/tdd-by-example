20d5ef5c3b0d1bbed5197d2bac6a2a2af96c36c2
Octavio Marchi Parisi <octaviomarchi@msn.com>
2019-11-18 14:19:42 -0800

    27. Addition, Finally

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,6 +1,6 @@
 package guru.springframework;
 
-public class Money {
+public class Money implements Expression{
 	protected int amount;
 	protected String currency;
 
@@ -36,6 +36,10 @@ public class Money {
 	
 	public Money times(int multiplier) {
 		return new Money(amount * multiplier, this.currency);
+	}
+	
+	public Expression plus(Money addend) {
+		return new Money(amount + addend.amount, currency);
 	}
 	
 }
