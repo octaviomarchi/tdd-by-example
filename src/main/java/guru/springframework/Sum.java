49f3cdd4a81fcd2ddb8e631e031c69a05c5d39d0
Octavio Marchi Parisi <octaviomarchi@msn.com>
2019-11-19 13:39:48 -0800

    31. Abstraction, Finally

--- src/main/java/guru/springframework/Sum.java
+++ src/main/java/guru/springframework/Sum.java
@@ -4,19 +4,24 @@ public class Sum implements Expression {
 	Expression augmend;
 	Expression addmend;
 
-    public Sum(Expression augmend, Expression addmend) {
-        this.augmend = augmend;
-        this.addmend = addmend;
-    }
+	public Sum(Expression augmend, Expression addmend) {
+		this.augmend = augmend;
+		this.addmend = addmend;
+	}
 
-    @Override
-    public Money reduce(Bank bank, String to) {
-        int amount = augmend.reduce(bank, to).amount + addmend.reduce(bank, to).amount;
-        return new Money(amount, to);
-    }
+	@Override
+	public Money reduce(Bank bank, String to) {
+		int amount = augmend.reduce(bank, to).amount + addmend.reduce(bank, to).amount;
+		return new Money(amount, to);
+	}
 
 	@Override
 	public Expression plus(Expression addend) {
-		return null;
+		return new Sum(this, addmend);
+	}
+
+	@Override
+	public Expression times(int multiplier) {
+		return new Sum(augmend.times(multiplier), addmend.times(multiplier));
 	}
 }
